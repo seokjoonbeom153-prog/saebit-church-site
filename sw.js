feed7322d7766b2f1fc83525f271393bd4af32ae
seokjoonbeom153-prog <seokjoonbeom153@gmail.com>
2026-09-12 20:32:41 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE='saebit-church-v1';
+const ASSETS=[
+  './',
+  './index.html',
+  './styles.css',
+  './manifest.webmanifest',
+  './app-icon.svg',
+  './bulletins/index.html',
+  './bible-reading/index.html',
+  './bible-reading/new-testament-week6.html'
+];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
+});
